8427ae262a39114cc6f46af33f4402cedbbe6693
Saltchy <36486751+Saltchy@users.noreply.github.com>
2018-02-15 13:26:19 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,24 +1,8 @@
 const Discord = require('discord.js');
-// const YTDL = require('ytdl-core');
 const weather = require('weather-js')
 const bot = new Discord.Client()
 
 var prefix = ("*")
-
-// function play(connection, message){
-//     var server = servers[message.guild.id];
-
-//     server.dispatcher = connection.playStream(ytdl(server.queue[0], {filter: "audioonly"}));
-
-//     server.queue.shift();
-
-//     server.dispatcher.on("end", function() {
-//         if (server.queue[0]) play(connection, message);
-//         else connection.disconnect();
-//     });
-// }
-
-// var servers = {};
 
 bot.on('ready', function () {
     bot.user.setGame("Command: *help");
@@ -141,43 +125,6 @@ bot.on('message', message => {
                 .addField('Humidity', `${current.humidity}%`, true)
 
             message.channel.send({embed});
-            // message.channel.send(JSON.stringify(result[0].current, null, 2));
         });
     }
-
-    // if (message.content === prefix + "play"){
-        
-    //     if (!args[1]){
-    //         message.channel.sendMessage("Please provide a link.");
-    //         return;
-    //     }
-
-    //     if (!message.member.voiceChannel){
-    //         message.channel.sendMessage("You must be in a voice channel");
-    //         return;
-    //     }
-    //     if (!servers[message.guild.id]) servers[message.guild.id] = {
-    //         queue: []
-    //     };
-
-    //         var server = servers[message.guild.id];
-
-    //         server.queue.push(args[1]);
-
-    //         if(!message.guild.voiceConnection) message.member.voiceChannel.join().then(function(connection){
-    //             play(connection, message);
-    //         });
-    // }
-
-    // if (message.content === prefix + "skip"){
-    //     var server = servers[message.guild.id];
-
-    //     if (server.dispatcher) server.dispatcher.end();
-    // }
-
-    // if(message.content === prefix + "stop"){
-    //     var server = servers[message.guild.id];
-
-    //     if (message.guild.voiceConnection) message.guild.voiceConnection.disconnect();
-    // }
 });
